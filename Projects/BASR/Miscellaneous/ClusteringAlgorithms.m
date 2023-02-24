0190f34c5a872b76fac6ad14f108b8bc310968ab
nabeelali1207 <nabeel1207ali@outlook.com>
2023-02-24 18:36:47 +0000

    1

--- Projects/BASR/Miscellaneous/ClusteringAlgorithms.m
+++ Projects/BASR/Miscellaneous/ClusteringAlgorithms.m
@@ -73,51 +73,188 @@ clc
 
 
 
-% % 2023.02.23
-% % K-Means implementation 2
+% % % 2023.02.23
+% % % K-Means implementation 2
+% 
+% load fisheriris
+% 
+% k = 2;
+% data = meas(:,3:4);
+% % rng default; % For reproducibility
+% % data = [randn(100,2)*0.75+ones(100,2);
+% %     randn(100,2)*0.5-ones(100,2)];
+% 
+% colors = ['r', 'g', 'b', 'm', 'k', 'c'];
+% symbols = ['x', '+', '*', 'o', 's', 'd'];
+% distance = zeros(size(data, 1), k);
+% [m, n] = size(data);
+% center = zeros(k, n);
+% for i = 1: k
+% 	center(i, :) =  data(i, :);
+% end
+% 
+% for i = 1: 20
+% 	for j = 1: size(center, 1)
+% 		tempCenter = center(j, :);
+% 		tempDistance = sqrt(sum((data - tempCenter) .^ 2, 2));
+% 		distance(:, j) = tempDistance;
+% 	end
+% 	
+% 	[M, I] = min(distance, [], 2);
+% 	
+% 	for j = 1: k
+% 		if not(isempty(data(I == j, :)))
+% 			center(j, :) = mean(data(I == j, :));
+% 		end
+% 	end
+% end
+% 
+% hold on
+% for i = 1: m
+% 	color_index = mod(i - 1, length(colors)) + 1;
+%     color = colors(color_index); 
+%     plot(data(I == i, 1), data(I == i, 2), '.', 'MarkerSize', 10, 'Color', color);
+% end
+% for i = 1: k
+% 	symbol_index = mod(i - 1, length(symbols)) + 1;
+% 	symbol = symbols(symbol_index);
+% 	plot(center(i, 1), center(i, 2), symbol, 'MarkerSize', 15)
+% end
+% hold off
 
-load fisheriris
 
-k = 2;
-data = meas(:,3:4);
-% rng default; % For reproducibility
-% data = [randn(100,2)*0.75+ones(100,2);
-%     randn(100,2)*0.5-ones(100,2)];
 
-colors = ['r', 'g', 'b', 'm', 'k', 'c'];
-symbols = ['x', '+', '*', 'o', 's', 'd'];
-distance = zeros(size(data, 1), k);
+
+
+
+
+
+
+
+% % % 2023.02.23
+% % % DBSCAN failed implementation 1
+% 
+% rng('default')
+% N = 300; 
+% r1 = 0.5;
+% r2 = 5;
+% theta = linspace(0, 2 * pi, N)';
+% X1 = r1 * [cos(theta), sin(theta)] + rand(N, 1);
+% X2 = r2 * [cos(theta), sin(theta)] + rand(N, 1);
+% 
+% data = [X1; X2];
+% eps = .5;
+% minPts = 5;
+% 
+% [m, n] = size(data);
+% distance = zeros(m, n);
+% dataInfo = [zeros(size(data, 1), 1) (-1 * ones(size(data, 1), 1))]; % column1 = visted or not, column2 = cluster group (-1 = none, 0 = noise)
+% clusterID = 0;
+% 
+% 
+% i = 1;
+% while any(dataInfo(:, 1) == 0)
+% 
+% 	if dataInfo(i, 1) == 1
+% 		i = i + 1;
+% 		continue;
+% 	end
+% 	
+% 	tempCenter = data(i, :);
+% 	distance = sqrt(sum((data - tempCenter) .^ 2, 2));
+% 	neighbors = find(distance <= eps);
+% 	
+% 	if (size(neighbors, 1) < minPts) && (dataInfo(i, 2) == -1)
+% 		dataInfo(i, 2) = 0;
+% 	elseif (size(neighbors, 1) > minPts) && (dataInfo(i, 2) == -1)
+% 		clusterID = clusterID + 1;
+% 		dataInfo(i, 2) = clusterID;
+% 		dataInfo(neighbors, 2) = clusterID;
+% 		[temp, i] = max(distance(neighbors));
+% 		continue;
+% 	end
+% 	
+% 	dataInfo(i, 1) = 1;
+% 	
+% 	i = i + 1;
+% 	if (i > size(dataInfo, 1)) && any(dataInfo(:, 1) == 0)
+% 		i = find(dataInfo(:, 1) == 0, 1, 'first');
+% 	end
+% end
+% 
+% plot(data(:,1),data(:,2), '.')
+
+
+
+
+
+
+
+
+
+% % 2023.02.24
+% % DBSCAN implementation 2
+
+rng('default')
+N = 300; 
+r1 = 0.5;
+r2 = 5;
+theta = linspace(0, 2 * pi, N)';
+X1 = r1 * [cos(theta), sin(theta)] + rand(N, 1);
+X2 = r2 * [cos(theta), sin(theta)] + rand(N, 1);
+
+dataIn = [X1; X2];
+eps = .1;
+minPts = 5;
+
+data = (dataIn - min(dataIn)) ./ (max(dataIn) - min(dataIn));
+
+buffer = [];
 [m, n] = size(data);
-center = zeros(k, n);
-for i = 1: k
-	center(i, :) =  data(i, :);
-end
+distance = zeros(m, n);
+dataInfo = [zeros(size(data, 1), 1) (-1 * ones(size(data, 1), 1))]; % column1 = visted or not, column2 = cluster group (-1 = none, 0 = noise)
+clusterID = 0;
 
-for i = 1: 20
-	for j = 1: size(center, 1)
-		tempCenter = center(j, :);
-		tempDistance = sqrt(sum((data - tempCenter) .^ 2, 2));
-		distance(:, j) = tempDistance;
+buffer(end + 1) = 1;
+
+while any(dataInfo(:, 1) == 0)
+
+	index = buffer(1);
+	
+	if dataInfo(index, 1) == 1
+		buffer = buffer(2: end);
+		continue;
+	else
+		dataInfo(index, 1) = 1;
 	end
 	
-	[M, I] = min(distance, [], 2);
+	tempCenter = data(index, :);
+	distance = sqrt(sum((data - tempCenter) .^ 2, 2));
+	neighbors = find(distance <= eps);
 	
-	for j = 1: k
-		if not(isempty(data(I == j, :)))
-			center(j, :) = mean(data(I == j, :));
+	if (size(neighbors, 1) < minPts) && (dataInfo(index, 2) == -1)
+		dataInfo(index, 2) = 0;
+	elseif (size(neighbors, 1) > minPts) && (dataInfo(index, 2) == -1)
+		if length(buffer) < 1
+			clusterID = clusterID + 1;
 		end
+		dataInfo(neighbors, 2) = clusterID;
+		[temp, tempIndex] = sort(distance(neighbors), 'descend');
+		buffer = [buffer; tempIndex];
 	end
+	
+	buffer = buffer(2: end);
 end
 
 hold on
-for i = 1: m
-	color_index = mod(i - 1, length(colors)) + 1;
-    color = colors(color_index); 
-    plot(data(I == i, 1), data(I == i, 2), '.', 'MarkerSize', 10, 'Color', color);
+for i = 1: clusterID
+	plot(data(dataInfo(:, 2), 1), data(dataInfo(:, 2), 2), '.')
 end
-for i = 1: k
-	symbol_index = mod(i - 1, length(symbols)) + 1;
-	symbol = symbols(symbol_index);
-	plot(center(i, 1), center(i, 2), symbol, 'MarkerSize', 15)
-end
-hold off
+
+
+
+
+
+
+
+
