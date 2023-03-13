911bdc9a906946d927a375a0c337fee3e8a96fc0
nabeelali1207 <nabeel1207ali@gmail.com>
2023-03-13 17:59:40 +0500

    updated TimeCardReader.py

--- Miscellaneous/TimeCardReader.py
+++ Miscellaneous/TimeCardReader.py
@@ -2,20 +2,39 @@ import csv
 import glob
 import os
 
+data = []
 
-# print(os.listdir())
-os.chdir("Miscellaneous/")
+def parseFile():
+	# os.chdir("Miscellaneous/")
+	csv_files = glob.glob("Miscellaneous/Time Card_*_export.csv")
+	print(csv_files)
+	for filename in csv_files:
+		with open(filename) as csvfile:
+			reader = csv.reader(csvfile)
+			print(reader)
+			for i, row in enumerate(reader):
+				if i != 0:
+					data.append(row)
+	
 
-# csv_files = glob.glob("Miscellaneous/Time Card_*_export.csv")
-csv_files = glob.glob("Time Card_*_export.csv")
-print(csv_files)
-# Loop through each CSV file and read the data
-for filename in csv_files:
-	print(filename)
-	with open(filename) as csvfile:
-		reader = csv.reader(csvfile)
-		print(reader)
-		for i, row in enumerate(reader):
-			# Process the row
-			if i != 0:
-				print(row)
+
+parseFile()
+data = [[row[3], row[4], row[5]] for row in data if len(row) >= 6]
+print(data)
+
+# # print(os.listdir())
+# os.chdir("Miscellaneous/")
+
+# # csv_files = glob.glob("Miscellaneous/Time Card_*_export.csv")
+# csv_files = glob.glob("Time Card_*_export.csv")
+# print(csv_files)
+# # Loop through each CSV file and read the data
+# for filename in csv_files:
+# 	print(filename)
+# 	with open(filename) as csvfile:
+# 		reader = csv.reader(csvfile)
+# 		print(reader)
+# 		for i, row in enumerate(reader):
+# 			# Process the row
+# 			if i != 0:
+# 				print(row)
